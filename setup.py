97fcafe66a7c2f7574f9e4f4e34953cbcc7a4449
jnmelchorg <jnmelchorguti@gmail.com>
2020-03-20 09:44:19 +0000

    update setup

--- setup.py
+++ setup.py
@@ -102,9 +102,9 @@ def parse_optional_arguments():
 def findglpkheaderpath():
     # Finding glpk header path
     pythonpath = os.path.split(sys.executable)[0]
-    if len(pythonpath.rsplit(r'/b', 1)) is not 0:
-        if pythonpath.rsplit(r'/b', 1)[1] is 'in':
-            pythonpath = pythonpath.rsplit(r'/b', 1)[0]
+    if len(pythonpath.rsplit('/b', 1)) is not 0:
+        if pythonpath.rsplit('/b', 1)[1] is 'in':
+            pythonpath = pythonpath.rsplit('/b', 1)[0]
     print(pythonpath)
     trypaths = [pythonpath+'\Library\include\glpk.h',\
                 pythonpath+'\include\glpk.h', \
@@ -123,9 +123,9 @@ def findglpkheaderpath():
 def findglpklibrarypath():   
     # Finding glpk header path
     pythonpath = os.path.split(sys.executable)[0]
-    if len(pythonpath.rsplit(r'/b', 1)) is not 0:
-        if pythonpath.rsplit(r'/b', 1)[1] is 'in':
-            pythonpath = pythonpath.rsplit(r'/b', 1)[0]
+    if len(pythonpath.rsplit('/b', 1)) is not 0:
+        if pythonpath.rsplit('/b', 1)[1] is 'in':
+            pythonpath = pythonpath.rsplit('/b', 1)[0]
     trypaths = [pythonpath+'\Library\lib\glpk.lib',\
                 pythonpath+'\libs\glpk.lib',\
                 pythonpath+'/Library/lib/glpk.lib',\
