3da717ffabaea3d5cb2ba144ce86b9f9be5227cc
Jose Nicolas Melchor gutierrez <jnmelchorguti@gmail.com>
2021-07-01 12:57:52 +0100

    change to version 0.0.3

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ def setup_package():
 
     metadata = dict(
         name="pyensys",
-        version='0.0.1',
+        version='0.0.3',
         url='https://github.com/jnmelchorg/pyensys',
         description='python energy systems simulator - pyensys.',
         author='Dr. Eduardo Alejandro Martínez Ceseña, \
