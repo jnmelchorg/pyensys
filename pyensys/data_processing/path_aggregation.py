4df78a3608574a69632b9433375338d24cec2638
Jose Nicolas Melchor gutierrez <jnmelchorguti@gmail.com>
2021-09-06 16:45:30 +0100

    data clustering for profiles using sklearn

---
+++ pyensys/data_processing/path_aggregation.py
@@ -0,0 +1,31 @@
+from sklearn.cluster import Birch
+from dataclasses import dataclass
+from pandas import DataFrame
+
+
+@dataclass
+class Birch_Settings:
+    threshold : float = 0.03
+    branching_factor : int = 50
+    n_clusters: int = 0
+
+class Clustering:
+
+    def _check_number_clusters(self, settings: Birch_Settings):
+        if settings.n_clusters > 0:
+            return settings.n_clusters
+        else:
+            return None
+
+    def set_up_birch_algorithm(self, settings: Birch_Settings):
+        number_clusters = self._check_number_clusters(settings)
+        self.algorithm = Birch(threshold = settings.threshold, 
+            branching_factor= settings.branching_factor,
+            n_clusters=number_clusters)
+    
+    def train_birch_algorithm(self, data: DataFrame):
+        self.algorithm.fit(data)
+
+    def perform_clustering(self, data: DataFrame):
+        return self.algorithm.fit_predict(data)
+
