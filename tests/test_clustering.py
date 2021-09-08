2afac962815a7eb9c56ef9e7deca8bc3743a4b4b
Jose Nicolas Melchor gutierrez <jnmelchorguti@gmail.com>
2021-09-08 09:58:11 +0100

    new tests for time series clustering

--- tests/test_clustering.py
+++ tests/test_clustering.py
@@ -1,9 +1,11 @@
+from datetime import time
 from pandas import read_excel, DataFrame
 from os.path import dirname, abspath, join
 from sklearn.cluster import Birch
-from numpy import ndarray,array_equal
+from numpy import ndarray,array_equal, allclose
 
-from pyensys.data_processing.clustering import Birch_Settings, Clustering
+from pyensys.data_processing.clustering import Birch_Settings, Clustering, \
+    TimeSeriesClustering
 
 def get_test_file_path() -> str:
     current_directory_path=dirname(abspath(__file__))
@@ -32,6 +34,11 @@ def initialise_birch_clustering_algorithm() -> Clustering:
     clusters.set_up_birch_algorithm(birch_algorithm_settings)
     return clusters
 
+def set_time_series(clustering: TimeSeriesClustering) -> TimeSeriesClustering:
+    data = read_normalized_demand_profiles()
+    clustering.set_time_series(data['Sheet1'])
+    return clustering
+
 
 
 
@@ -52,3 +59,37 @@ def test_perform_birch_clustering():
     data = get_training_data()
     clusters.train_birch_algorithm(data)
     assert array_equal(clusters.perform_clustering(data), [0, 1, 2, 2, 3, 4, 3, 5])
+
+def test_set_time_series():
+    data = read_normalized_demand_profiles()
+    time_clustering = TimeSeriesClustering()
+    time_clustering.set_time_series(data['Sheet1'])
+    assert isinstance(time_clustering.time_series, DataFrame)
+
+def test_row_dataframe_to_numpy():
+    TEST_ROW: int = 45
+    time_clustering = TimeSeriesClustering()
+    time_clustering = set_time_series(time_clustering)
+    row = time_clustering.time_series.iloc[TEST_ROW]
+    row = time_clustering._process_row(row)
+    assert isinstance(row, ndarray)
+
+def test_initialise_birch_clustering_algorithm():
+    time_clustering = TimeSeriesClustering()
+    time_clustering.initialise_birch_clustering_algorithm(Birch_Settings())
+    assert time_clustering.clusters.algorithm.threshold == 0.03
+
+def test_perform_time_series_clustering():
+    time_clustering = TimeSeriesClustering()
+    time_clustering = set_time_series(time_clustering)
+    time_clustering.initialise_birch_clustering_algorithm(Birch_Settings())
+    time_clustering.perform_clustering()
+    assert len(time_clustering.time_series_clusters) != 0
+
+def test_calculate_clusters_centroids():
+    time_clustering = TimeSeriesClustering()
+    time_clustering = set_time_series(time_clustering)
+    time_clustering.initialise_birch_clustering_algorithm(Birch_Settings())
+    time_clustering.perform_clustering()
+    time_clustering.calculate_clusters_centroids()
+    assert len(time_clustering.clusters_centroids) != 0
