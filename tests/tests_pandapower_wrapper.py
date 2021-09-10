ec0c5f9ae2f8fda9553cd35fb53ca26fd91faf93
Jose Nicolas Melchor gutierrez <jnmelchorguti@gmail.com>
2021-09-10 16:06:45 +0100

    tests update

--- tests/tests_pandapower_wrapper.py
+++ tests/tests_pandapower_wrapper.py
@@ -1,163 +1,159 @@
-from pandapower.timeseries.data_sources.frame_data import DFData
-from pyensys.wrappers.pandapower import pandapower_wrapper, profile, \
-    time_series_output_file, output_variable_set, simulation_settings
+from pyensys.wrappers.pandapower import PandapowerWrapper, Profile, \
+    TimeSeriesOutputFileSettings, OutputVariableSet, SimulationSettings
 
 from pandas import DataFrame
 from numpy.random import random
 
-from pandapower import create_empty_network, create_bus, \
-    create_ext_grid, create_line, create_transformer, create_load, create_gen, \
-    pandapowerNet, create_pwl_cost
-from pandapower.control import ConstControl
 from pandapower.timeseries.output_writer import OutputWriter
+from pandapower import runopp
 
 from typing import List
+from math import isclose
 
 HOURS_IN_DAY: int = 24
-DUMMY_POWER: float = 25
+DUMMY_POWER_GENERATION: float = 250
+DUMMY_POWER_DEMAND: float = 90
 
-def create_dummy_buses(network: pandapowerNet) -> List[int]:
-    buses = []
-    buses.append(create_bus(network, 110))
-    buses.append(create_bus(network, 110))
-    buses.append(create_bus(network, 20))
-    buses.append(create_bus(network, 20))
-    buses.append(create_bus(network, 20))
-    return buses
-
-def create_dummy_lines(network: pandapowerNet, buses: List[int]):
-    create_line(network, buses[0], buses[1], 10, "149-AL1/24-ST1A 110.0")
-    create_line(network, buses[2], buses[3], 10, "184-AL1/30-ST1A 20.0")
-    create_line(network, buses[2], buses[4], 10, "184-AL1/30-ST1A 20.0")
-
-def create_dummy_network() -> pandapowerNet:
-    """
-    simple net that looks like:
-
-    ext_grid b0---b1 trafo(110/20) b2----b3 load
-                                    |
-                                    |
-                                    b4 sgen
-    """
-    net = create_empty_network()
-    buses = create_dummy_buses(net)
-    create_ext_grid(net, buses[0])
-    create_transformer(net, buses[1], buses[2], "25 MVA 110/20 kV", name='tr1')
-    create_dummy_lines(net, buses)
-    create_load(net, buses[3], p_mw=20., q_mvar=10., name='load1')
-    create_gen(net, buses[4], max_p_mw =25., min_p_mw=0.0, \
-        max_q_mvar=0.2, min_q_mvar=-0.2, name='sgen1', min_vm_pu=0.9, 
-        max_vm_pu=1.1, p_mw=20.0)
-    create_pwl_cost(net, 0, "gen", [[0.0, 25.0, 1.0]])
-    return net
+def load_test_network() -> PandapowerWrapper:
+    wrapper = PandapowerWrapper()
+    file = 'C:\\Users\\f09903jm\\git projects\\pyensys\\tests\\matpower\\case9.mat'
+    wrapper.load_mpc_file_to_pandapower(file, 60.0)
+    return wrapper
 
 def create_dummy_profile_data() -> DataFrame:
     profiles = DataFrame()
-    profiles['load1_p'] = random(HOURS_IN_DAY) * DUMMY_POWER
-    profiles['sgen1_p'] = random(HOURS_IN_DAY) * DUMMY_POWER
+    profiles['load1_p'] = [67.28095505, 9.65466896, 11.70181664, \
+        3.43219975, 32.61755449, 84.85360346, 29.95258074, \
+        39.20088948, 42.54484985, 20.04279764, 71.8828131, \
+        41.96986409, 7.21552544, 25.88618743, 26.54954622, \
+        17.21125963, 87.53580282, 87.63197474, 69.49124619, \
+        1.3286569, 10.60700364, 54.86348133, 13.76146653, 18.54152069]
+    profiles['gen1_p'] = [240.44092015, 205.50525905, 18.7321705, \
+        136.89180324, 227.91854659, 243.98374614, 20.22587417, \
+        204.13000456, 142.52170518, 69.15390821, 75.08460741, \
+        100.18795258, 177.58383221, 105.98171684, 111.27365188, \
+        177.83779845, 163.92025682, 148.67520413, \
+        28.95077739, 116.63621831, 126.36468424, 164.58739745, \
+        71.51992487, 77.18482911]
     return profiles
 
 def create_dummy_profile_object(data: DataFrame, indexes: list, 
-    profile_name: list, variable_name: str, element_type: str) -> profile:
-    wrapper = pandapower_wrapper()
-    prof = profile(data=wrapper.create_profiles(data))
+    profile_name: list, variable_name: str, element_type: str) -> Profile:
+    prof = Profile(data=data.loc[:, profile_name[0]].to_frame())
     prof.components_indexes_in_power_system = indexes
     prof.name = profile_name
     prof.variable_name = variable_name
     prof.components_type = element_type
     return prof
 
-def create_dummy_profiles() -> list:
+def create_dummy_profiles() -> List[Profile]:
     profiles = []
     data = create_dummy_profile_data()
     profiles.append(create_dummy_profile_object(data, [0], ["load1_p"],\
         'p_mw', 'load'))
-    profiles.append(create_dummy_profile_object(data, [0], ["sgen1_p"],\
+    profiles.append(create_dummy_profile_object(data, [0], ["gen1_p"],\
         'p_mw', 'gen'))
     return profiles
 
-def create_dummy_output_file_settings() -> time_series_output_file:
-    output_settings = time_series_output_file()
+def create_dummy_output_file_settings() -> TimeSeriesOutputFileSettings:
+    output_settings = TimeSeriesOutputFileSettings()
     output_settings.directory = \
        'C:\\Users\\f09903jm\\git projects\\pyensys\\tests\\outputs'
     output_settings.number_time_steps = HOURS_IN_DAY
     output_settings.format = '.xlsx'
     return output_settings
 
-def create_pandapower_writer(network: pandapowerNet) -> OutputWriter:
-    wrapper = pandapower_wrapper()
+def create_pandapower_writer(wrapper: PandapowerWrapper) -> PandapowerWrapper:
     output_settings = create_dummy_output_file_settings()
-    return wrapper.create_output_writer(network, output_settings)
+    variables_to_save = create_dummy_output_variables()
+    wrapper.create_output_writer(output_settings, variables_to_save)
+    return wrapper
 
-def create_dummy_output_variables() -> List[output_variable_set]:
+def create_dummy_output_variables() -> List[OutputVariableSet]:
     variables = []
-    variables.append(output_variable_set('res_load', 'p_mw', []))
-    variables.append(output_variable_set('res_bus', 'vm_pu', []))
-    variables.append(output_variable_set('res_line', 'loading_percent', []))
-    variables.append(output_variable_set('res_line', 'i_ka', []))
+    variables.append(OutputVariableSet('res_load', 'p_mw', []))
+    variables.append(OutputVariableSet('res_bus', 'vm_pu', []))
+    variables.append(OutputVariableSet('res_line', 'loading_percent', []))
+    variables.append(OutputVariableSet('res_line', 'i_ka', []))
     return variables
 
-def add_dummy_variables_to_writer(writer: OutputWriter) -> OutputWriter:
-    variables = create_dummy_output_variables()
-    for variable in variables:
-        writer.log_variable(variable.name_dataset, variable.name_variable,
-            index=variable.variable_indexes,
-            eval_function=variable.evaluation_function)
-    return writer
-
-def create_dummy_controllers(network: pandapowerNet):
-    wrapper = pandapower_wrapper()
+def create_dummy_controllers(wrapper: PandapowerWrapper) -> PandapowerWrapper:
     profiles = create_dummy_profiles()
-    for profile in profiles:
-        wrapper.create_controller(network, profile)
+    wrapper.create_controllers(profiles)
+    return wrapper
 
-def create_dummy_network_with_settings() -> pandapowerNet:
-    network = create_dummy_network()
-    create_dummy_controllers(network)
-    writer = create_pandapower_writer(network)
-    writer = add_dummy_variables_to_writer(writer)
-    return network
+def create_dummy_network_with_settings() -> PandapowerWrapper:
+    wrapper = load_test_network()
+    wrapper = create_dummy_controllers(wrapper)
+    wrapper = create_pandapower_writer(wrapper)
+    return wrapper
 
-def create_dummy_simulation_settings() -> simulation_settings:
-    settings = simulation_settings()
+def create_dummy_simulation_settings() -> SimulationSettings:
+    settings = SimulationSettings()
     settings.time_steps = range(HOURS_IN_DAY)
-    settings.display_progress_bar = True
-    settings.continue_on_divergence = True
+    settings.display_progress_bar = False
+    settings.continue_on_divergence = False
     settings.optimisation_software = 'pypower'
     return settings
 
 
 def test_if_load_mpc_file_to_pandapower_works():
-    wrapper = pandapower_wrapper()
-    file = 'C:\\Users\\f09903jm\\git projects\\pyensys\\tests\\matpower\\case9.mat'
-    assert wrapper.load_mpc_file_to_pandapower(file, 50.0)
-
-def test_create_pandapower_dataframe():
-    wrapper = pandapower_wrapper()
-    profiles = create_dummy_profile_data()
-    assert isinstance(wrapper.create_profiles(profiles['load1_p']), DFData)
+    wrapper = PandapowerWrapper()
+    file = 'C:\\Users\\f09903jm\\git projects\\pyensys\\tests\\matpower\\case5.mat'
+    wrapper.load_mpc_file_to_pandapower(file, 50.0)
+    assert len(wrapper.network['bus'].index) == 5
 
 def test_create_controller():
-    wrapper = pandapower_wrapper()
-    network = create_dummy_network()
+    wrapper = load_test_network()
     profiles = create_dummy_profiles()
-    assert isinstance(wrapper.create_controller(network, profiles[0]), ConstControl)
+    wrapper._create_controller(profiles[0])
+    data = wrapper.network['controller'].iat[0, 0].data_source.df
+    profile_name = wrapper.network['controller'].iat[0, 0].profile_name
+    assert len(data.index) == 24
+    assert profile_name == ['load1_p']
 
-def test_output_writer():
-    network = create_dummy_network()
-    assert(create_pandapower_writer(network), OutputWriter)
+test_create_controller()
+
+def test_create_controllers():
+    wrapper = load_test_network()
+    profiles = create_dummy_profiles()
+    wrapper.create_controllers(profiles)
+    data_demand_profile = wrapper.network['controller'].iat[0, 0].data_source.df
+    data_generation_profile = wrapper.network['controller'].iat[1, 0].data_source.df
+    profile_load_name = wrapper.network['controller'].iat[0, 0].profile_name
+    profile_generation_name = wrapper.network['controller'].iat[1, 0].profile_name
+    assert len(wrapper.network['controller'].index) == 2
+    assert profile_load_name == ['load1_p'] and profile_generation_name == ['gen1_p']
+    assert len(data_demand_profile.index) == 24 and len(data_generation_profile.index) == 24
 
 def test_log_variable_in_writer():
-    network = create_dummy_network()
-    writer = create_pandapower_writer(network)
-    wrapper = pandapower_wrapper()
-    variable_set = output_variable_set('res_bus', 'p_mw', [])
-    wrapper.log_variables_in_output_writer(writer, variable_set)
-    assert writer.log_variables[0] == ('res_bus', 'p_mw', None, None, None)
+    wrapper = load_test_network()
+    writer = OutputWriter(wrapper.network)
+    variable_set = OutputVariableSet('res_bus', 'p_mw', [])
+    wrapper._log_variable_in_output_writer(writer, variable_set)
+    output_writer_class = wrapper.network['output_writer'].iat[0, 0]
+    assert output_writer_class.log_variables[2] == ('res_bus', 'p_mw', None, None, None)
+
+def test_log_variables_in_writer():
+    wrapper = load_test_network()
+    writer = OutputWriter(wrapper.network)
+    variables_to_save = [OutputVariableSet('res_bus', 'p_mw', []), \
+        OutputVariableSet('res_line', 'i_ka', [])]
+    wrapper._log_variables_in_output_writer(writer, variables_to_save)
+    output_writer_class = wrapper.network['output_writer'].iat[0, 0]
+    assert output_writer_class.log_variables[2] == ('res_bus', 'p_mw', None, None, None) and \
+        output_writer_class.log_variables[3] == ('res_line', 'i_ka', None, None, None)
+
+def test_output_writer():
+    wrapper = load_test_network()
+    wrapper = create_pandapower_writer(wrapper)
+    output_writer_class = wrapper.network['output_writer'].iat[0, 0]
+    assert output_writer_class.log_variables[2] == \
+        ('res_line', 'loading_percent', None, None, None)
 
 def test_run_timestep_simulation():
-    wrapper = pandapower_wrapper()
-    network = create_dummy_network_with_settings()
+    wrapper = create_dummy_network_with_settings()
     settings = create_dummy_simulation_settings()
-    wrapper.run_timestep_simulation(network, settings)
-    assert (network.OPF_converged == True or network.OPF_converged == False)
+    wrapper.run_timestep_simulation(settings)
+    assert wrapper.network.OPF_converged == True
+    assert isclose(3714.91365, wrapper.network.res_cost, abs_tol=1e-4)
