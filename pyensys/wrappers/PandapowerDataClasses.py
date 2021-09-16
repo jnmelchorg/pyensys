82b7f1da7637a0edc0b98b9a56995d03adcff641
jnmelchorg <jnmelchorguti@gmail.com>
2021-09-16 16:36:40 +0100

    dataclasses separated from wrapper

---
+++ pyensys/wrappers/PandapowerDataClasses.py
@@ -0,0 +1,32 @@
+from pandas import DataFrame
+from dataclasses import dataclass, field
+from typing import List
+
+@dataclass
+class Profile:
+    components_indexes_in_power_system: List[int] = field(default_factory=list)
+    data: DataFrame = field(default_factory=DataFrame)
+    column_names: List[str] = field(default_factory=list)
+    variable_name: str = ''
+    components_type:str = ''
+
+@dataclass
+class TimeSeriesOutputFileSettings:
+    number_time_steps: int = 0
+    directory: str = ''
+    format: str = ''
+
+@dataclass
+class OutputVariableSet:
+    name_dataset: str = ''
+    name_variable: str = ''
+    variable_indexes : List[int] = field(default_factory=list)
+    evaluation_function = None
+
+@dataclass
+class SimulationSettings:
+    display_progress_bar: bool = False
+    optimisation_software: str = ''
+    opf_type: str = ''
+    continue_on_divergence: bool = False
+    time_steps: List[int] = field(default_factory=list)
--- pyensys/wrappers/pandapower.py
+++ pyensys/wrappers/pandapower.py
@@ -1,94 +1,62 @@
-from os import name
 from pandapower.converter import from_mpc
 from pandapower.timeseries import DFData
 from pandapower.control import ConstControl
 from pandapower.timeseries.output_writer import OutputWriter
-from pandapower import runopp, runpm_ac_opf
+from pandapower import runopp, runpm_ac_opf, create_empty_network, pandapowerNet
 from pandapower.timeseries.run_time_series import run_timeseries
-
-from pandas import DataFrame
-
-from dataclasses import dataclass, field
-
 from typing import List
+from pyensys.wrappers.PandapowerDataClasses import *
 
-optimal_power_flow_software_options = {
-    'pypower' : runopp,
-    'powermodels' : runpm_ac_opf
+OPTIMAL_POWER_FLOW_SOFTWARE_OPTIONS = {
+    "ac": {
+        'pypower' : runopp,
+        'powermodels' : runpm_ac_opf
+    }
 }
 
-@dataclass
-class Profile:
-    components_indexes_in_power_system: list = field(default_factory=list)
-    data: DataFrame = field(default_factory=DataFrame)
-    name: list = field(default_factory=list)
-    variable_name: str = ''
-    components_type:str = ''
+class PandaPowerWrapper:
+    def __init__(self):
+        self.network = create_empty_network()
 
-@dataclass
-class TimeSeriesOutputFileSettings:
-    number_time_steps: int = 0
-    directory: str = ''
-    format: str = ''
-
-@dataclass
-class OutputVariableSet:
-    name_dataset: str = ''
-    name_variable: str = ''
-    variable_indexes : list = field(default_factory=list)
-    evaluation_function = None
-
-@dataclass
-class SimulationSettings:
-    display_progress_bar: bool = False
-    optimisation_software: str = ''
-    continue_on_divergence: str = False
-    time_steps: list = field(default_factory=list)
-
-class PandapowerWrapper:
-
-    nodes_list = []
-
-    def load_mpc_file_to_pandapower(self, filename_with_extension: str,
+    def load_mat_file_to_pandapower(self, filename_with_extension: str,
         frequency_hz: float):
         self.network = from_mpc(filename_with_extension, f_hz=frequency_hz)
 
-    def create_controllers(self, profiles: List[Profile]):
+    def add_controllers_to_network(self, profiles: List[Profile]):
         for profile in profiles:
-            self._create_controller(profile)
-    
-    def _create_controller(self, profile: Profile):
+            self.add_controller_to_network(profile)
+
+    def add_controller_to_network(self,profile: Profile):
         ConstControl(self.network, element=profile.components_type, 
             element_index=profile.components_indexes_in_power_system,
             variable=profile.variable_name,
             data_source=DFData(profile.data),
-            profile_name=profile.name)
-    
+            profile_name=profile.column_names)
 
-    def create_output_writer(self, output_properties: TimeSeriesOutputFileSettings, \
+    def add_output_writer_to_network(self, output_properties: TimeSeriesOutputFileSettings, \
         variables_to_save: List[OutputVariableSet]):
         range_timesteps = range(0, output_properties.number_time_steps)
         writer = OutputWriter(self.network, range_timesteps, 
             output_path=output_properties.directory, 
             output_file_type=output_properties.format, log_variables=list())
-        self._log_variables_in_output_writer(writer, variables_to_save)
-        
-    def _log_variables_in_output_writer(self, writer: OutputWriter, 
-        variables_to_save: List[OutputVariableSet]):
+        writer = self.log_variables_in_output_writer(writer, variables_to_save)
+
+    def log_variables_in_output_writer(self, writer: OutputWriter, \
+        variables_to_save: List[OutputVariableSet]) -> OutputWriter:
         for variable in variables_to_save:
-            writer = self._log_variable_in_output_writer(writer, variable)
-    
-    def _log_variable_in_output_writer(self, writer: OutputWriter, 
+            writer = self.log_variable_in_output_writer(writer, variable)
+        return writer
+
+    def log_variable_in_output_writer(self, writer: OutputWriter, 
         variable_set: OutputVariableSet) -> OutputWriter:
         writer.log_variable(variable_set.name_dataset, variable_set.name_variable, 
             index=(variable_set.variable_indexes if variable_set.variable_indexes \
             else None), eval_function=variable_set.evaluation_function)
         return writer
 
-
     def run_timestep_simulation(self, settings: SimulationSettings):
+        available_software_per_type = OPTIMAL_POWER_FLOW_SOFTWARE_OPTIONS[settings.opf_type]
         run_timeseries(self.network, time_steps=settings.time_steps,
             continue_on_divergence=settings.continue_on_divergence,
             verbose=settings.display_progress_bar, 
-            run=optimal_power_flow_software_options[settings.optimisation_software])
-    
+            run=available_software_per_type[settings.optimisation_software])
