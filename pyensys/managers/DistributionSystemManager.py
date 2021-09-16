e28e2b8bac1327ec2a40989f34aa18c47c1642f8
Jose Nicolas Melchor gutierrez <jnmelchorguti@gmail.com>
2021-09-16 06:53:46 +0100

    new method identify and solve problem

--- pyensys/managers/DistributionSystemManager.py
+++ pyensys/managers/DistributionSystemManager.py
@@ -1,2 +1,5 @@
-class DistributionSystem:
-    pass
+from pyensys.readers.ReaderDataClasses import Parameters
+
+def identify_and_solve(parameters: Parameters):
+    if parameters.problem_settings.problem_optimizer == "recursive_function":
+        pass
