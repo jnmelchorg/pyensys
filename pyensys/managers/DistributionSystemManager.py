468588de8b269e4efa18a6374e61853daca29c87
Jose Nicolas Melchor gutierrez <jnmelchorguti@gmail.com>
2021-09-20 17:19:02 +0100

    initial definition of method identify and solve

--- pyensys/managers/DistributionSystemManager.py
+++ pyensys/managers/DistributionSystemManager.py
@@ -1,5 +1,8 @@
 from pyensys.readers.ReaderDataClasses import Parameters
+from pyensys.Optimisers.RecursiveFunction import RecursiveFunction
 
 def identify_and_solve(parameters: Parameters):
     if parameters.problem_settings.problem_optimizer == "recursive_function":
-        pass
+        RF = RecursiveFunction()
+        RF.initialise(parameters)
+        RF.solve()
