c3bcc55f6949dcd093cb280212cbc18244376a25
Jose Nicolas Melchor gutierrez <jnmelchorguti@gmail.com>
2021-09-27 21:26:00 +0100

    methods and tests to define graph created

--- pyensys/Optimisers/RecursiveFunction.py
+++ pyensys/Optimisers/RecursiveFunction.py
@@ -1,8 +1,11 @@
-from pandas.core.frame import DataFrame
+from pandas import DataFrame, Series
 from pyensys.wrappers.PandaPowerManager import PandaPowerManager
-from pyensys.readers.ReaderDataClasses import Parameters, PandaPowerProfilesData
-from typing import List
+from pyensys.readers.ReaderDataClasses import Parameters, PandaPowerProfilesData, \
+    OptimisationProfileData, OptimisationProfilesData
+from pyensys.data_processing.clustering import TimeSeriesClustering, Birch_Settings
+from typing import List, Dict
 from dataclasses import dataclass, field
+from networkx import DiGraph
 
 @dataclass
 class DataMultiplier:
@@ -15,14 +18,28 @@ class DataMultipliers:
     multipliers: List[DataMultiplier] = field(default_factory=list)
     initialised: bool = False
 
+@dataclass
+class GraphNode:
+    scenarios: List[int] = field(default_factory=list)
+    multiplier: float = -1.0
+    level: int = -1
+
+@dataclass
+class GraphNodes:
+    nodes: Dict[int, GraphNode] = field(default_factory=dict)
+    initialised: bool = False
+
 class RecursiveFunction:
 
     def __init__(self):
-        self.graph_nodes_multipliers: List[DataMultipliers] = []
+        self.graphs_multipliers: List[DataMultipliers] = []
         self.original_pp_profiles_data = PandaPowerProfilesData()
+        self.graphs: List[DiGraph] = []
+        self.parameters = Parameters()
         
     def operational_check(self):
-        if self.opt_optimizer == "pandapower" and self.intertemporal:
+        if self.parameters.problem_settings.opf_optimizer == "pandapower" and \
+            self.parameters.problem_settings.intertemporal:
             self.pp_opf.run_timestep_opf_pandapower()
     
     def update_pandapower_controllers(self, current_node: int):
@@ -45,20 +62,101 @@ class RecursiveFunction:
         return profile_position_to_update
 
     def get_data_multipliers_current_node(self, current_node: int) -> DataMultipliers:
-        return self.graph_nodes_multipliers[current_node]
+        return self.graphs_multipliers[current_node]
         
     def initialise(self, parameters: Parameters):
+        self.parameters = parameters
         if parameters.problem_settings.opf_optimizer == "pandapower":
             self.pp_opf = PandaPowerManager()
-            self.opt_optimizer = "pandapower"
-            self.intertemporal = parameters.problem_settings.intertemporal
             self.pp_opf.initialise_pandapower_network(parameters)
             self.original_pp_profiles_data = parameters.pandapower_profiles_data
-    
+            profiles_clusters = create_profiles_clusters(parameters)
+            for profile in profiles_clusters.data:
+                self.create_graph(profile)
+
     def solve(self):
         self.operational_check()
 
+    def create_graph(self, profile: OptimisationProfileData):
+        graph_nodes = determine_graph_nodes(profile.data)
+        self.graphs_multipliers.append(get_graph_multipliers(graph_nodes, profile))
+        edges = determine_graph_edges(graph_nodes)
+        graph = DiGraph()
+        graph.add_edges_from(edges)
+        self.graphs.append(graph)
 
+def create_profile_clusters(profile: OptimisationProfileData) -> DataFrame:
+    clusters = TimeSeriesClustering()
+    clusters.set_time_series(profile.data)
+    clusters.initialise_birch_clustering_algorithm(Birch_Settings())
+    clusters.perform_clustering()
+    clusters.calculate_clusters_centroids()
+    return DataFrame(data=clusters.clusters_centroids, columns=profile.data.columns)
+
+def create_profiles_clusters(parameters: Parameters) -> OptimisationProfilesData:
+    series_to_clusters: OptimisationProfilesData = OptimisationProfilesData(initialised=True)
+    if parameters.optimisation_profiles_data.initialised:
+        for profile in parameters.optimisation_profiles_data.data:
+            data = create_profile_clusters(profile)
+            series_to_clusters.data.append(OptimisationProfileData(element_type=profile.element_type, \
+                variable_name=profile.variable_name, data=data))
+    return series_to_clusters
+
+def determine_graph_nodes_in_row(dataframe_row: Series) -> List[GraphNode]:
+    stored_multipliers: Dict[float, int] = {}
+    new_nodes: List[GraphNode] = []
+    for column, (_, value) in enumerate(dataframe_row.iteritems()):
+        if stored_multipliers.get(value, None) is not None:
+            new_nodes[stored_multipliers[value]].scenarios.append(column)
+        else:
+            stored_multipliers[value] = len(new_nodes)
+            new_nodes.append(GraphNode(scenarios=[column], multiplier=value))
+    return new_nodes
+
+def get_graph_multipliers(graph_nodes: GraphNodes, profile: OptimisationProfileData) -> DataMultipliers:
+    multipliers = DataMultipliers()
+    multipliers.multipliers = [DataMultiplier() for _ in graph_nodes.nodes.keys()]
+    if graph_nodes.initialised:
+        for position, node_info in graph_nodes.nodes.items():
+            multipliers.multipliers[position].data_multiplier = node_info.multiplier
+            multipliers.multipliers[position].element_type = profile.element_type
+            multipliers.multipliers[position].variable_name = profile.variable_name
+    return multipliers
+
+def determine_graph_nodes(data: DataFrame) -> GraphNodes:
+    nodes = GraphNodes()
+    nodes.initialised = True
+    node_counter = -1
+    level = -1
+    for index in data.index:
+        new_nodes = determine_graph_nodes_in_row(data.loc[index])
+        level += 1
+        for node in new_nodes:
+            node_counter += 1
+            node.level = level
+            nodes.nodes[node_counter] = node
+    return nodes
+
+def get_nodes_per_level(graph_nodes: GraphNodes) -> Dict[int, List[int]]:
+    nodes_per_level: Dict[int, List[int]] = {}
+    for position, node_info in graph_nodes.nodes.items():
+        if nodes_per_level.get(node_info.level, None) is None:
+            nodes_per_level[node_info.level] = [position]
+        else:
+            nodes_per_level[node_info.level].append(position)
+    return nodes_per_level
+
+def determine_graph_edges(graph_nodes: GraphNodes) -> List[List[int]]:
+    edges: List[List[int]] = []
+    if graph_nodes.initialised:
+        nodes_per_level = get_nodes_per_level(graph_nodes)
+        for level in range(len(nodes_per_level) - 1):
+            for current_node in nodes_per_level[level]:
+                for sucessor in nodes_per_level[level + 1]:
+                    if any(item in graph_nodes.nodes[current_node].scenarios for item in \
+                        graph_nodes.nodes[sucessor].scenarios):
+                        edges.append([current_node, sucessor])
+    return edges
 
 
         
--- pyensys/tests/Optimisers/tests_RecursiveFunction.py
+++ pyensys/tests/Optimisers/tests_RecursiveFunction.py
@@ -1,11 +1,13 @@
-from numpy.lib.npyio import load
-from pyensys.Optimisers.RecursiveFunction import RecursiveFunction, DataMultipliers, DataMultiplier
-from pyensys.readers.ReaderDataClasses import Parameters, PandaPowerProfileData, OutputVariable
-from pyensys.tests.tests_data_paths import get_path_case9_mat, set_pandapower_test_output_directory
-from pandas import DataFrame, date_range
+from pyensys.Optimisers.RecursiveFunction import *
+from pyensys.readers.ReaderDataClasses import Parameters, PandaPowerProfileData, OutputVariable, \
+    OptimisationProfileData
+from pyensys.tests.tests_data_paths import get_path_case9_mat, set_pandapower_test_output_directory, \
+    get_clustering_data_test
+from pandas import DataFrame, date_range, read_excel, option_context
 from math import isclose
 from typing import List
 from copy import copy
+from numpy import allclose
 
 def load_test_case() -> Parameters:
     parameters = Parameters()
@@ -37,6 +39,14 @@ def load_test_case() -> Parameters:
     parameters.problem_settings.opf_type = "ac"
     parameters.problem_settings.opf_optimizer = "pandapower"
     parameters.problem_settings.intertemporal = True
+    parameters.optimisation_profiles_data.data = [
+        OptimisationProfileData(\
+            element_type="load", variable_name="p_mw", data=read_excel(io=\
+            get_clustering_data_test(), sheet_name="Sheet1")),
+            OptimisationProfileData(\
+            element_type="gen", variable_name="p_mw", data=read_excel(io=\
+            get_clustering_data_test(), sheet_name="Sheet2"))]
+    parameters.optimisation_profiles_data.initialised = True
     return parameters
 
 def load_data_multipliers() -> List[DataMultipliers]:
@@ -79,11 +89,11 @@ def test_get_data_multipliers_current_node():
     data_multipliers.multipliers = [\
         DataMultiplier(data_multiplier=0.1, element_type="load", variable_name="p_mw"), \
         DataMultiplier(data_multiplier=0.2, element_type="load", variable_name="p_mw")]
-    RF.graph_nodes_multipliers.append(data_multipliers)
+    RF.graphs_multipliers.append(data_multipliers)
     data_multipliers.multipliers = [\
         DataMultiplier(data_multiplier=0.3, element_type="load", variable_name="p_mw"), \
         DataMultiplier(data_multiplier=0.4, element_type="load", variable_name="p_mw")]
-    RF.graph_nodes_multipliers.append(data_multipliers)
+    RF.graphs_multipliers.append(data_multipliers)
     assert RF.get_data_multipliers_current_node(1).multipliers[0].data_multiplier == 0.3
 
 def test_get_profile_position_to_update():
@@ -94,14 +104,81 @@ def test_get_profile_position_to_update():
     PROFILE_POSITION = RF.get_profile_position_to_update(list_multipliers[1].multipliers[1])
     assert PROFILE_POSITION == 1
 
+def test_create_profile_clusters():
+    parameters = load_test_case()
+    profile_clusters = create_profile_clusters(parameters.optimisation_profiles_data.data[0])
+    with option_context('display.max_rows', None, 'display.max_columns', None):  # more options can be specified also
+        print(profile_clusters)
+        print(profile_clusters.nunique(axis=1).tolist())
+    EXPECTED_NUMBER_UNIQUE_VALUES = [1, 1, 1, 2, 3, 2, 3]
+    EXPECTED_VALUES_ROW_4 = [0.85, 1.04, 1.4]
+    assert profile_clusters.shape == (7, 3)
+    assert profile_clusters.nunique(axis=1).tolist() == EXPECTED_NUMBER_UNIQUE_VALUES
+    assert allclose(profile_clusters.loc[4].tolist(), EXPECTED_VALUES_ROW_4)
 
+def test_create_profiles_clusters():
+    parameters = load_test_case()
+    profiles_clusters = create_profiles_clusters(parameters)
+    assert len(profiles_clusters.data) == 2
+    assert (profiles_clusters.data[1].variable_name == "p_mw")
+    assert (profiles_clusters.data[1].element_type == "gen")
 
 def test_update_pandapower_controllers():
     RF = RecursiveFunction()
     parameters = load_test_case()
     RF.initialise(parameters)
-    RF.graph_nodes_multipliers = load_data_multipliers()
+    RF.graphs_multipliers = load_data_multipliers()
     RF.update_pandapower_controllers(1)
     RESULT = RF.pp_opf.wrapper.network['controller'].iat[1, 0].data_source.df
     assert DataFrame(data=[[96], [80], [7.2]], columns=['gen1_p']).equals(RESULT)
 
+def test_determine_graph_nodes_in_row():
+    parameters = load_test_case()
+    profiles_clusters = create_profiles_clusters(parameters)
+    nodes = determine_graph_nodes_in_row(profiles_clusters.data[0].data.loc[3])
+    assert nodes[0].scenarios == [0] and nodes[1].scenarios == [1, 2]
+    assert nodes[0].multiplier == 0.9 and nodes[1].multiplier == 1.025
+
+def test_determine_graph_nodes():
+    parameters = load_test_case()
+    profiles_clusters = create_profiles_clusters(parameters)
+    graph_nodes = determine_graph_nodes(profiles_clusters.data[0].data)
+    assert graph_nodes.initialised
+    assert len(graph_nodes.nodes) == 13
+    assert graph_nodes.nodes[0].scenarios == [0, 1, 2]
+    assert graph_nodes.nodes[3].scenarios == [0] and graph_nodes.nodes[4].scenarios == [1, 2]
+    assert graph_nodes.nodes[8].multiplier == 0.98
+    assert graph_nodes.nodes[6].level == 4
+
+def test_get_nodes_per_level():
+    parameters = load_test_case()
+    profiles_clusters = create_profiles_clusters(parameters)
+    graph_nodes = determine_graph_nodes(profiles_clusters.data[0].data)
+    nodes_per_level = get_nodes_per_level(graph_nodes)
+    assert len(nodes_per_level) == 7
+    assert nodes_per_level[4] == [5, 6, 7] and nodes_per_level[6] == [10, 11, 12]
+
+def test_determine_graph_edges():
+    parameters = load_test_case()
+    profiles_clusters = create_profiles_clusters(parameters)
+    graph_nodes = determine_graph_nodes(profiles_clusters.data[0].data)
+    edges = determine_graph_edges(graph_nodes)
+    assert len(edges) == 13
+    assert edges[2] == [2, 3] and edges[6] == [4, 7] and edges[11] == [8, 11]
+
+def test_get_graph_multipliers():
+    parameters = load_test_case()
+    profiles_clusters = create_profiles_clusters(parameters)
+    graph_nodes = determine_graph_nodes(profiles_clusters.data[0].data)
+    multipliers = get_graph_multipliers(graph_nodes, profiles_clusters.data[0])
+    assert len(multipliers.multipliers) == 13
+    assert multipliers.multipliers[0].data_multiplier == 1.0 and \
+        multipliers.multipliers[6].data_multiplier == 1.04
+
+def test_create_graph():
+    parameters = load_test_case()
+    profiles_clusters = create_profiles_clusters(parameters)
+    RF = RecursiveFunction()
+    RF.create_graph(profiles_clusters.data[0])
+    assert RF.graphs[0].number_of_nodes() == 13
+    assert len(RF.graphs_multipliers[0].multipliers) == 13
