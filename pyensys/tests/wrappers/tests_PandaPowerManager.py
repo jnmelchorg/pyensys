446d02757286641d3fb2398809fb7e944f8c6dcf
Jose Nicolas Melchor gutierrez <jnmelchorguti@gmail.com>
2021-11-08 21:04:14 +0000

    extracting code to load case 9 in tests

--- pyensys/tests/wrappers/tests_PandaPowerManager.py
+++ pyensys/tests/wrappers/tests_PandaPowerManager.py
@@ -7,6 +7,32 @@ from pandas import DataFrame, date_range
 from math import isclose
 from unittest.mock import MagicMock
 
+def _load_test_parameter_case_9():
+    parameters = Parameters()
+    parameters.pandapower_mpc_settings.mat_file_path = get_path_case9_mat()
+    parameters.pandapower_mpc_settings.system_frequency = 50.0
+    parameters.pandapower_mpc_settings.initialised =  True
+    parameters.pandapower_profiles_data.initialised = True
+    parameters.pandapower_profiles_data.data = [\
+        PandaPowerProfileData(element_type="load", variable_name="p_mw", \
+        indexes=[0], data=DataFrame(data=[[67.28095505], [9.65466896], [11.70181664]], \
+        columns=['load1_p']), active_columns_names=['load1_p'])]
+    parameters.output_settings.initialised = True
+    parameters.output_settings.output_variables = [OutputVariable(name_dataset='res_bus', \
+        name_variable='p_mw', variable_indexes=[]), OutputVariable(name_dataset='res_line', \
+        name_variable='i_ka', variable_indexes=[])]
+    parameters.output_settings.directory = set_pandapower_test_output_directory()
+    parameters.output_settings.format = ".xlsx"
+    parameters.opf_time_settings.initialised = True
+    parameters.opf_time_settings.date_time_settings = \
+        date_range(start="00:00:00", end="02:00:00", freq="H")
+    parameters.pandapower_optimisation_settings.initialised = True
+    parameters.pandapower_optimisation_settings.continue_on_divergence = False
+    parameters.pandapower_optimisation_settings.display_progress_bar = False
+    parameters.pandapower_optimisation_settings.optimisation_software = 'pypower'
+    parameters.problem_settings.initialised = True
+    parameters.problem_settings.opf_type = "ac"
+    return parameters
 
 def test_load_mat_file_to_pandapower_case1():
     manager = PandaPowerManager()
@@ -264,30 +290,7 @@ def test_initialise_pandapower_network():
 
 def test_run_timestep_opf_pandapower():
     manager = PandaPowerManager()
-    parameters = Parameters()
-    parameters.pandapower_mpc_settings.mat_file_path = get_path_case9_mat()
-    parameters.pandapower_mpc_settings.system_frequency = 50.0
-    parameters.pandapower_mpc_settings.initialised =  True
-    parameters.pandapower_profiles_data.initialised = True
-    parameters.pandapower_profiles_data.data = [\
-        PandaPowerProfileData(element_type="load", variable_name="p_mw", \
-        indexes=[0], data=DataFrame(data=[[67.28095505], [9.65466896], [11.70181664]], \
-        columns=['load1_p']), active_columns_names=['load1_p'])]
-    parameters.output_settings.initialised = True
-    parameters.output_settings.output_variables = [OutputVariable(name_dataset='res_bus', \
-        name_variable='p_mw', variable_indexes=[]), OutputVariable(name_dataset='res_line', \
-        name_variable='i_ka', variable_indexes=[])]
-    parameters.output_settings.directory = set_pandapower_test_output_directory()
-    parameters.output_settings.format = ".xlsx"
-    parameters.opf_time_settings.initialised = True
-    parameters.opf_time_settings.date_time_settings = \
-        date_range(start="00:00:00", end="02:00:00", freq="H")
-    parameters.pandapower_optimisation_settings.initialised = True
-    parameters.pandapower_optimisation_settings.continue_on_divergence = False
-    parameters.pandapower_optimisation_settings.display_progress_bar = False
-    parameters.pandapower_optimisation_settings.optimisation_software = 'pypower'
-    parameters.problem_settings.initialised = True
-    parameters.problem_settings.opf_type = "ac"
+    parameters = _load_test_parameter_case_9()
     manager.initialise_pandapower_network(parameters)
     manager.run_timestep_opf_pandapower()
     assert manager.wrapper.network.OPF_converged == True
