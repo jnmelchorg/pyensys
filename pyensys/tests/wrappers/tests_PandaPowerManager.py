9853290267f260683884760e89b9b7802672a832
jnmelchorg <jnmelchorguti@gmail.com>
2021-11-09 15:35:24 +0000

    updating parameter inputs in powermanager class in tests

--- pyensys/tests/wrappers/tests_PandaPowerManager.py
+++ pyensys/tests/wrappers/tests_PandaPowerManager.py
@@ -1,3 +1,4 @@
+from numpy.testing._private.utils import assert_equal
 from pyensys.wrappers.PandaPowerManager import PandaPowerManager
 from pyensys.readers.ReaderDataClasses import Parameters, PandaPowerProfileData, OutputVariable, \
     PandaPowerProfilesData
@@ -40,14 +41,16 @@ def test_load_mat_file_to_pandapower_case1():
     parameters.pandapower_mpc_settings.mat_file_path = get_path_case9_mat()
     parameters.pandapower_mpc_settings.system_frequency = 50.0
     parameters.pandapower_mpc_settings.initialised =  True
-    manager.load_mat_file_to_pandapower(parameters)
+    manager._parameters = parameters
+    manager.load_mat_file_to_pandapower()
     assert len(manager.wrapper.network['bus'].index) == 9
 
 def test_load_mat_file_to_pandapower_case2():
     manager = PandaPowerManager()
     parameters = Parameters()
     parameters.pandapower_mpc_settings.initialised =  False
-    manager.load_mat_file_to_pandapower(parameters)
+    manager._parameters = parameters
+    manager.load_mat_file_to_pandapower()
     assert len(manager.wrapper.network['bus'].index) == 0
 
 def test_add_controllers_to_network_case1():
@@ -58,7 +61,8 @@ def test_add_controllers_to_network_case1():
         PandaPowerProfileData(element_type="load", variable_name="p_mw", \
         indexes=[0], data=DataFrame(data=[[67.28095505], [9.65466896], [11.70181664]], \
         columns=['load1_p']), active_columns_names=['load1_p'])]
-    manager.add_controllers_to_network(parameters)
+    manager._parameters = parameters
+    manager.add_controllers_to_network()
     data = manager.wrapper.network['controller'].iat[0, 0].data_source.df
     profile_name = manager.wrapper.network['controller'].iat[0, 0].profile_name
     assert len(data.index) == 3
@@ -68,7 +72,8 @@ def test_add_controllers_to_network_case2():
     manager = PandaPowerManager()
     parameters = Parameters()
     parameters.pandapower_profiles_data.initialised = False
-    manager.add_controllers_to_network(parameters)
+    manager._parameters = parameters
+    manager.add_controllers_to_network()
     assert manager.wrapper.network['controller'].empty
 
 def test_define_output_variables():
@@ -77,7 +82,8 @@ def test_define_output_variables():
     parameters.output_settings.output_variables = [OutputVariable(name_dataset='res_bus', \
         name_variable='p_mw', variable_indexes=[]), OutputVariable(name_dataset='res_line', \
         name_variable='i_ka', variable_indexes=[])]
-    variables = manager._define_output_variables(parameters)
+    manager._parameters = parameters
+    variables = manager._define_output_variables()
     assert len(variables) == 2
     assert variables[0].name_dataset == 'res_bus'
     assert variables[1].name_variable == 'i_ka'
@@ -89,7 +95,8 @@ def test_define_output_settings():
     parameters.output_settings.format = ".xlsx"
     parameters.opf_time_settings.date_time_settings = \
         date_range(start="00:00:00", end="02:00:00", freq="H")
-    settings = manager._define_output_settings(parameters)
+    manager._parameters = parameters
+    settings = manager._define_output_settings()
     assert settings.directory == set_pandapower_test_output_directory()
     assert settings.format == ".xlsx"
     assert settings.number_time_steps == 3
@@ -106,7 +113,8 @@ def test_add_output_writer_to_network_case1():
     parameters.opf_time_settings.initialised = True
     parameters.opf_time_settings.date_time_settings = \
         date_range(start="00:00:00", end="02:00:00", freq="H")
-    manager.add_output_writer_to_network(parameters)
+    manager._parameters = parameters
+    manager.add_output_writer_to_network()
     output_writer_class = manager.wrapper.network['output_writer'].iat[0, 0]
     assert len(output_writer_class.log_variables) == 2
     assert output_writer_class.log_variables[1] == ('res_line', 'i_ka', None, None, None)
@@ -118,7 +126,8 @@ def test_add_output_writer_to_network_case2():
     parameters.opf_time_settings.initialised = True
     parameters.opf_time_settings.date_time_settings = \
         date_range(start="00:00:00", end="02:00:00", freq="H")
-    manager.add_output_writer_to_network(parameters)
+    manager._parameters = parameters
+    manager.add_output_writer_to_network()
     assert not manager.wrapper.network.get('output_writer', False)
 
 def test_add_output_writer_to_network_case3():
@@ -131,7 +140,8 @@ def test_add_output_writer_to_network_case3():
     parameters.output_settings.directory = set_pandapower_test_output_directory()
     parameters.output_settings.format = ".xlsx"
     parameters.opf_time_settings.initialised = False
-    manager.add_output_writer_to_network(parameters)
+    manager._parameters = parameters
+    manager.add_output_writer_to_network()
     assert not manager.wrapper.network.get('output_writer', False)
 
 def test_add_output_writer_to_network_case4():
@@ -139,7 +149,8 @@ def test_add_output_writer_to_network_case4():
     parameters = Parameters()
     parameters.output_settings.initialised = False
     parameters.opf_time_settings.initialised = False
-    manager.add_output_writer_to_network(parameters)
+    manager._parameters = parameters
+    manager.add_output_writer_to_network()
     assert not manager.wrapper.network.get('output_writer', False)
 
 def test_define_simulation_settings_case1():
@@ -154,7 +165,8 @@ def test_define_simulation_settings_case1():
     parameters.opf_time_settings.initialised = True
     parameters.opf_time_settings.date_time_settings = \
         date_range(start="00:00:00", end="02:00:00", freq="H")
-    manager.define_simulation_settings(parameters)
+    manager._parameters = parameters
+    manager.define_simulation_settings()
     assert len(manager.simulation_settings.time_steps) == 3
     assert manager.simulation_settings.opf_type == "ac"
     assert manager.simulation_settings.optimisation_software == "pypower"
@@ -168,7 +180,8 @@ def test_define_simulation_settings_case2():
     parameters.opf_time_settings.initialised = True
     parameters.opf_time_settings.date_time_settings = \
         date_range(start="00:00:00", end="02:00:00", freq="H")
-    manager.define_simulation_settings(parameters)
+    manager._parameters = parameters
+    manager.define_simulation_settings()
     assert len(manager.simulation_settings.time_steps) == 0
     assert manager.simulation_settings.opf_type == ""
     assert manager.simulation_settings.optimisation_software == ""
@@ -184,7 +197,8 @@ def test_define_simulation_settings_case3():
     parameters.opf_time_settings.initialised = True
     parameters.opf_time_settings.date_time_settings = \
         date_range(start="00:00:00", end="02:00:00", freq="H")
-    manager.define_simulation_settings(parameters)
+    manager._parameters = parameters
+    manager.define_simulation_settings()
     assert len(manager.simulation_settings.time_steps) == 0
     assert manager.simulation_settings.opf_type == ""
     assert manager.simulation_settings.optimisation_software == ""
@@ -199,7 +213,8 @@ def test_define_simulation_settings_case4():
     parameters.problem_settings.initialised = True
     parameters.problem_settings.opf_type = "ac"
     parameters.opf_time_settings.initialised = False
-    manager.define_simulation_settings(parameters)
+    manager._parameters = parameters
+    manager.define_simulation_settings()
     assert len(manager.simulation_settings.time_steps) == 0
     assert manager.simulation_settings.opf_type == ""
     assert manager.simulation_settings.optimisation_software == ""
@@ -212,7 +227,8 @@ def test_define_simulation_settings_case5():
     parameters.opf_time_settings.initialised = True
     parameters.opf_time_settings.date_time_settings = \
         date_range(start="00:00:00", end="02:00:00", freq="H")
-    manager.define_simulation_settings(parameters)
+    manager._parameters = parameters
+    manager.define_simulation_settings()
     assert len(manager.simulation_settings.time_steps) == 0
     assert manager.simulation_settings.opf_type == ""
     assert manager.simulation_settings.optimisation_software == ""
@@ -224,7 +240,8 @@ def test_define_simulation_settings_case6():
     parameters.problem_settings.initialised = True
     parameters.problem_settings.opf_type = "ac"
     parameters.opf_time_settings.initialised = False
-    manager.define_simulation_settings(parameters)
+    manager._parameters = parameters
+    manager.define_simulation_settings()
     assert len(manager.simulation_settings.time_steps) == 0
     assert manager.simulation_settings.opf_type == ""
     assert manager.simulation_settings.optimisation_software == ""
@@ -238,7 +255,8 @@ def test_define_simulation_settings_case7():
     parameters.pandapower_optimisation_settings.optimisation_software = 'pypower'
     parameters.problem_settings.initialised = False
     parameters.opf_time_settings.initialised = False
-    manager.define_simulation_settings(parameters)
+    manager._parameters = parameters
+    manager.define_simulation_settings()
     assert len(manager.simulation_settings.time_steps) == 0
     assert manager.simulation_settings.opf_type == ""
     assert manager.simulation_settings.optimisation_software == ""
@@ -249,7 +267,8 @@ def test_define_simulation_settings_case8():
     parameters.pandapower_optimisation_settings.initialised = False
     parameters.problem_settings.initialised = False
     parameters.opf_time_settings.initialised = False
-    manager.define_simulation_settings(parameters)
+    manager._parameters = parameters
+    manager.define_simulation_settings()
     assert len(manager.simulation_settings.time_steps) == 0
     assert manager.simulation_settings.opf_type == ""
     assert manager.simulation_settings.optimisation_software == ""
@@ -257,36 +276,22 @@ def test_define_simulation_settings_case8():
 def test_initialise_pandapower_network():
     manager = PandaPowerManager()
     parameters = Parameters()
-    parameters.pandapower_mpc_settings.mat_file_path = get_path_case9_mat()
-    parameters.pandapower_mpc_settings.system_frequency = 50.0
-    parameters.pandapower_mpc_settings.initialised =  True
-    parameters.pandapower_profiles_data.initialised = True
-    parameters.pandapower_profiles_data.data = [\
-        PandaPowerProfileData(element_type="load", variable_name="p_mw", \
-        indexes=[0], data=DataFrame(data=[[67.28095505], [9.65466896], [11.70181664]], \
-        columns=['load1_p']), active_columns_names=['load1_p'])]
-    parameters.output_settings.initialised = True
-    parameters.output_settings.output_variables = [OutputVariable(name_dataset='res_bus', \
-        name_variable='p_mw', variable_indexes=[]), OutputVariable(name_dataset='res_line', \
-        name_variable='i_ka', variable_indexes=[])]
-    parameters.output_settings.directory = set_pandapower_test_output_directory()
-    parameters.output_settings.format = ".xlsx"
-    parameters.opf_time_settings.initialised = True
-    parameters.opf_time_settings.date_time_settings = \
-        date_range(start="00:00:00", end="02:00:00", freq="H")
-    parameters.pandapower_optimisation_settings.initialised = True
-    parameters.pandapower_optimisation_settings.continue_on_divergence = False
-    parameters.pandapower_optimisation_settings.display_progress_bar = False
-    parameters.pandapower_optimisation_settings.optimisation_software = 'pypower'
-    parameters.problem_settings.initialised = True
-    parameters.problem_settings.opf_type = "ac"
+    parameters.problem_settings.opf_optimizer = "TEST"
+    manager._initialise = MagicMock()
     manager.initialise_pandapower_network(parameters)
-    assert len(manager.wrapper.network['bus'].index) == 9
-    assert len(manager.wrapper.network['controller'].iat[0, 0].data_source.df.index) == 3
-    assert len(manager.wrapper.network['output_writer'].iat[0, 0].log_variables) == 2
-    assert len(manager.simulation_settings.time_steps) == 3
-    assert manager.simulation_settings.opf_type == "ac"
-    assert manager.simulation_settings.optimisation_software == "pypower"
+    manager._initialise.assert_called_once()
+    assert_equal(manager._parameters.problem_settings.opf_optimizer, "TEST")
+
+def tes_initialise():
+    manager = PandaPowerManager()
+    manager.load_mat_file_to_pandapower = MagicMock()
+    manager.add_controllers_to_network = MagicMock()
+    manager.add_output_writer_to_network = MagicMock()
+    manager.define_simulation_settings = MagicMock()
+    manager.load_mat_file_to_pandapower.assert_called_once()
+    manager.add_controllers_to_network.assert_called_once()
+    manager.add_output_writer_to_network.assert_called_once()
+    manager.define_simulation_settings.assert_called_once()    
 
 def test_run_timestep_opf_pandapower():
     manager = PandaPowerManager()
@@ -307,7 +312,8 @@ def test_update_network_controllers_case1():
         PandaPowerProfileData(element_type="gen", variable_name="p_mw", \
             indexes=[0], data=DataFrame(data=[[240.44092015], [205.50525905], [18.7321705]], \
             columns=['gen1_p']), active_columns_names=['gen1_p'])]
-    manager.add_controllers_to_network(parameters)
+    manager._parameters = parameters
+    manager.add_controllers_to_network()
     pp_profiles = PandaPowerProfilesData()
     pp_profiles.initialised = True
     pp_profiles.data = [\
@@ -329,7 +335,8 @@ def test_update_network_controllers_case2():
         PandaPowerProfileData(element_type="gen", variable_name="p_mw", \
             indexes=[0], data=DataFrame(data=[[240.44092015], [205.50525905], [18.7321705]], \
             columns=['gen1_p']), active_columns_names=['gen1_p'])]
-    manager.add_controllers_to_network(parameters)
+    manager._parameters = parameters
+    manager.add_controllers_to_network()
     pp_profiles = PandaPowerProfilesData()
     pp_profiles.initialised = False
     manager.update_network_controllers(pp_profiles)
