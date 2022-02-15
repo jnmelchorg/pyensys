0d441d3bb03fd892f9ec90d27b5cac3b9f971e8f
Jose Nicolas Melchor gutierrez <jnmelchorguti@gmail.com>
2022-02-15 10:20:54 +0000

    new method to verify feasibility of the current node when all available interventions for that year are used. New method to get all available interventions for the current year

--- pyensys/Optimisers/NonAnticipativeRecursiveFunction.py
+++ pyensys/Optimisers/NonAnticipativeRecursiveFunction.py
@@ -1,6 +1,5 @@
+from pyensys.DataContainersInterface.AbstractDataContainer import AbstractDataContainer
 from pyensys.Optimisers.RecursiveFunction import InterIterationInformation, RecursiveFunction
-
-from  typing import List
 
 class NonAnticipativeRecursiveFunction(RecursiveFunction):
     def _verify_feasibility_of_solution_in_successor_nodes(self, \
@@ -9,7 +8,8 @@ class NonAnticipativeRecursiveFunction(RecursiveFunction):
             inter_iteration_information):
             return True
         else:
-            pass
+            return self._verify_feasibility_of_sucessor_with_all_available_interventions_for_current_year(\
+                inter_iteration_information)
     
     def _verify_feasibility_of_sucessor_with_no_new_interventions(self, \
         inter_iteration_information: InterIterationInformation) -> bool:
@@ -17,5 +17,20 @@ class NonAnticipativeRecursiveFunction(RecursiveFunction):
         return self._is_opf_feasible()
     
     def _get_available_interventions_for_current_year(self, \
-        inter_iteration_information: InterIterationInformation) -> List[str]:
-        return []
+        inter_iteration_information: InterIterationInformation) -> AbstractDataContainer:
+        viable_interventions = self._calculate_available_interventions(inter_iteration_information)
+        for key, value in self._calculate_available_interventions(inter_iteration_information):
+            if value.installation_time > 0:
+                viable_interventions.pop(key)        
+        return viable_interventions
+
+    def _verify_feasibility_of_sucessor_with_all_available_interventions_for_current_year(self, \
+        inter_iteration_information: InterIterationInformation) -> bool:
+        all_available_interventions = self._get_available_interventions_for_current_year(\
+            inter_iteration_information)
+        inter_iteration_information.new_interventions.extend(all_available_interventions)
+        self._operational_check(inter_iteration_information)
+        for key, _ in all_available_interventions:
+            inter_iteration_information.new_interventions.pop(key)
+        return self._is_opf_feasible()
+
