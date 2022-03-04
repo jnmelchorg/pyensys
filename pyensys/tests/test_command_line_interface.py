51c325ce0747898180c8649f0d20a68a3bc06553
Jose Nicolas Melchor gutierrez <jnmelchorguti@gmail.com>
2022-03-04 17:17:57 +0000

    format changes

--- pyensys/tests/test_command_line_interface.py
+++ pyensys/tests/test_command_line_interface.py
@@ -2,7 +2,8 @@ from pyensys.cli import pyensys_entry_point
 from pyensys.tests.test_data_paths import get_path_pandapower_json_test_data
 from click.testing import CliRunner
 
+
 def test_pyensys_entry_point():
     runner = CliRunner()
-    runner.invoke(pyensys_entry_point, 
-        [get_path_pandapower_json_test_data()])
+    runner.invoke(pyensys_entry_point,
+                  [get_path_pandapower_json_test_data()])
