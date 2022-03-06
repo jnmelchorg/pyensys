9a51c5be7361235ffdd18397fce89d0136f0c05a
Jose Nicolas Melchor gutierrez <jnmelchorguti@gmail.com>
2022-03-06 19:40:35 +0000

    Creating dummy input and output in main access function. The content of this method will change once the algorithm is completed but the input/output format will remain

--- pyensys/managers/GeneralManager.py
+++ pyensys/managers/GeneralManager.py
@@ -1,10 +1,16 @@
-from pyensys.readers.ReaderManager import read_parameters
-from pyensys.managers.DistributionSystemManager import identify_and_solve
+# from pyensys.readers.ReaderManager import read_parameters
+# from pyensys.managers.DistributionSystemManager import identify_and_solve
+from json import load, dump
+from os.path import dirname, join, abspath
+
 
 def main_access_function(file_path: str):
-    parameters = read_parameters(file_path)
-    if parameters.problem_settings.system == "distribution":
-        identify_and_solve(parameters)
-
-        
-        
+    # parameters = read_parameters(file_path)
+    # identify_and_solve(parameters)
+    path = join(dirname(__file__), "..", "tests", "json", "Output_Data_Investment_proposal.json")
+    with open(path) as output_data:
+        output = load(output_data)
+    with open(file_path) as input_data:
+        parameters = load(input_data)
+    with open(parameters["output_directory"] + "\\solution.json", "w") as output_dir:
+        dump(output, output_dir, ensure_ascii=False, indent=2)
