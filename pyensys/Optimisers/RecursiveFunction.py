1f8c728177f6912684e4f55b13823f1751cffd1d
Jose Nicolas Melchor gutierrez <jnmelchorguti@gmail.com>
2022-03-07 20:20:28 +0000

    format fix and static method extracted

--- pyensys/Optimisers/RecursiveFunction.py
+++ pyensys/Optimisers/RecursiveFunction.py
@@ -11,6 +11,7 @@ from itertools import combinations
 from copy import deepcopy
 from typing import Iterator, Tuple
 
+
 @dataclass
 class InterIterationInformation:
     incumbent_interventions: AbstractDataContainer = \
@@ -35,6 +36,7 @@ class InterIterationInformation:
     current_graph_node: int = 0
     level_in_graph: int = 0
 
+
 @dataclass
 class BinaryVariable:
     element_id: str = ""
@@ -47,15 +49,25 @@ class BinaryVariable:
     def __eq__(self, other):
         if isinstance(other, BinaryVariable):
             return (self.element_id == other.element_id) and (self.cost == other.cost) and \
-                (self.element_position == other.element_position) and \
-                (self.element_type == other.element_type) and (self.variable_name == other.variable_name)
+                   (self.element_position == other.element_position) and \
+                   (self.element_type == other.element_type) and (self.variable_name == other.variable_name)
+
 
 @dataclass
 class InterventionsInformation:
-    interventions: AbstractDataContainer  = \
+    interventions: AbstractDataContainer = \
         field(default_factory=lambda: AbstractDataContainer())
     path: str = ''
     year: int = 0
+
+
+def check_if_candidate_path_has_been_stored_in_incumbent(inter_iteration_information: InterIterationInformation) -> \
+        str:
+    for key, value in inter_iteration_information.incumbent_graph_paths:
+        if inter_iteration_information.candidate_solution_path in value:
+            return key
+    return "not found"
+
 
 class RecursiveFunction:
 
@@ -65,14 +77,14 @@ class RecursiveFunction:
         self._pool_interventions = AbstractDataContainer()
         self._opf = PandaPowerManager()
         self.__DAYS_PER_YEAR = 365
-        
+
     def initialise(self, parameters: Parameters):
         self._parameters = parameters
         self._create_control_graph()
         self._create_pool_interventions()
         if self._parameters.problem_settings.opf_optimizer == "pandapower":
             self._initialise_pandapower()
-    
+
     def _initialise_pandapower(self):
         self._opf.initialise_pandapower_network(self._parameters)
         self.original_pp_profiles_data = self._parameters.pandapower_profiles_data
@@ -80,54 +92,54 @@ class RecursiveFunction:
     def _create_control_graph(self):
         control_graph = RecursiveFunctionGraphCreator()
         self._control_graph = control_graph.create_recursive_function_graph(self._parameters)
-    
+
     def _create_pool_interventions(self):
         self._pool_interventions.create_list()
         counter = 0
         for variables in self._parameters.optimisation_binary_variables:
             for cost, position, id, time in zip(variables.costs, variables.elements_positions, \
-                variables.elements_ids, variables.installation_time):
-                self._pool_interventions.append(str(counter), BinaryVariable(\
-                    element_type=variables.element_type, variable_name=variables.variable_name,\
+                                                variables.elements_ids, variables.installation_time):
+                self._pool_interventions.append(str(counter), BinaryVariable( \
+                    element_type=variables.element_type, variable_name=variables.variable_name, \
                     element_id=id, element_position=position, cost=cost, installation_time=time))
                 counter += 1
 
     def solve(self, inter_iteration_information: InterIterationInformation):
         self._operational_check(inter_iteration_information)
         if self._is_opf_feasible():
-            inter_iteration_information = self._construction_of_solution(\
+            inter_iteration_information = self._construction_of_solution( \
                 inter_iteration_information)
             # Optimality Check
-            if not any(True for _ in self._control_graph.graph.neighbours(\
-                inter_iteration_information.current_graph_node)):
+            if not any(True for _ in self._control_graph.graph.neighbours( \
+                    inter_iteration_information.current_graph_node)):
                 self._optimality_check(inter_iteration_information)
                 return
             # Intervention Handler
             self._interventions_handler(inter_iteration_information)
 
     def _interventions_handler(self, inter_iteration_information: InterIterationInformation):
-        _available_interventions = self._calculate_available_interventions(\
+        _available_interventions = self._calculate_available_interventions( \
             inter_iteration_information)
         self._graph_exploration(inter_iteration_information)
         for number_combinations in range(1, len(_available_interventions) + 1):
-            for combinations in self._calculate_all_combinations(\
-                _available_interventions, number_combinations):
+            for combinations in self._calculate_all_combinations( \
+                    _available_interventions, number_combinations):
                 self._add_new_interventions_from_combinations(inter_iteration_information, \
-                    combinations)
+                                                              combinations)
                 # Graph exploration
                 self._graph_exploration(inter_iteration_information)
-    
+
     def _add_new_interventions_from_combinations(self, \
-        inter_iteration_information: InterIterationInformation, combination):
+                                                 inter_iteration_information: InterIterationInformation, combination):
         inter_iteration_information.new_interventions = AbstractDataContainer()
         inter_iteration_information.new_interventions.create_list()
         for element in combination:
             inter_iteration_information.new_interventions.extend(element)
-    
+
     def _graph_exploration(self, inter_iteration_information: InterIterationInformation):
         parent_node = inter_iteration_information.current_graph_node
-        for neighbour in self._control_graph.graph.neighbours(\
-            inter_iteration_information.current_graph_node):
+        for neighbour in self._control_graph.graph.neighbours( \
+                inter_iteration_information.current_graph_node):
             inter_iteration_information.level_in_graph += 1
             inter_iteration_information.current_graph_node = neighbour
             self.solve(inter_iteration_information=inter_iteration_information)
@@ -140,13 +152,13 @@ class RecursiveFunction:
 
     def _update_status_elements_opf(self, inter_iteration_information: InterIterationInformation):
         if len(inter_iteration_information.new_interventions) > 0:
-            self._update_status_elements_opf_per_intervention_group(\
+            self._update_status_elements_opf_per_intervention_group( \
                 inter_iteration_information.candidate_interventions)
-            self._update_status_elements_opf_per_intervention_group(\
+            self._update_status_elements_opf_per_intervention_group( \
                 inter_iteration_information.new_interventions)
         else:
             return
-    
+
     def _update_status_elements_opf_per_intervention_group(self, interventions: AbstractDataContainer):
         parameters_to_update = []
         for _, intervention in interventions:
@@ -176,60 +188,60 @@ class RecursiveFunction:
 
     def _run_opf(self):
         if self._parameters.problem_settings.opf_optimizer == "pandapower" and \
-            self._parameters.problem_settings.intertemporal:
+                self._parameters.problem_settings.intertemporal:
             self._opf.run_time_step_opf_pandapower()
 
     def _construction_of_solution(self, inter_iteration_information: InterIterationInformation) \
-        -> InterIterationInformation:
-        inter_iteration_information.candidate_solution_path.append(str(len(\
+            -> InterIterationInformation:
+        inter_iteration_information.candidate_solution_path.append(str(len( \
             inter_iteration_information.candidate_solution_path)), \
             inter_iteration_information.current_graph_node)
-        inter_iteration_information.candidate_interventions.append(\
+        inter_iteration_information.candidate_interventions.append( \
             str(inter_iteration_information.level_in_graph), \
             deepcopy(inter_iteration_information.new_interventions))
-        inter_iteration_information.candidate_operation_cost.append(\
+        inter_iteration_information.candidate_operation_cost.append( \
             str(inter_iteration_information.level_in_graph), \
             self._get_total_operation_cost())
         return inter_iteration_information
 
     def _calculate_available_interventions(self, inter_iteration_information: \
-        InterIterationInformation) -> AbstractDataContainer:
+            InterIterationInformation) -> AbstractDataContainer:
         _available_interventions = deepcopy(self._pool_interventions)
         for _, previous in inter_iteration_information.candidate_interventions:
-            _available_interventions = difference_abstract_data_containers(\
+            _available_interventions = difference_abstract_data_containers( \
                 _available_interventions, previous)
-        _available_interventions = difference_abstract_data_containers(\
+        _available_interventions = difference_abstract_data_containers( \
             _available_interventions, \
             inter_iteration_information.new_interventions)
         return _available_interventions
 
     def _calculate_all_combinations(self, available_interventions: AbstractDataContainer, \
-        length_set: int) -> Iterator[Tuple[int]]:
+                                    length_set: int) -> Iterator[Tuple[int]]:
         return combinations(available_interventions, length_set)
-   
-    def _create_new_pandapower_profiles(self, current_graph_node: int)\
-         -> PandaPowerProfilesData:
+
+    def _create_new_pandapower_profiles(self, current_graph_node: int) \
+            -> PandaPowerProfilesData:
         new_profiles = PandaPowerProfilesData(initialised=True)
         for modifier in self._control_graph.nodes_data[current_graph_node]:
             new_profiles.data.append(self._create_new_pandapower_profile(modifier))
         return new_profiles
-    
+
     def _create_new_pandapower_profile(self, modifier_info: ClusterData) -> PandaPowerProfileData:
-        profile = deepcopy(self.original_pp_profiles_data.data[\
-            self._get_profile_position_to_update(modifier_info)])
+        profile = deepcopy(self.original_pp_profiles_data.data[ \
+                               self._get_profile_position_to_update(modifier_info)])
         profile.data = profile.data * modifier_info.centroid
         return profile
-    
+
     def _get_profile_position_to_update(self, modifier_info: ClusterData) -> int:
         for position, pp_profile in enumerate(self.original_pp_profiles_data.data):
             if modifier_info.element_type == pp_profile.element_type and \
-                modifier_info.variable_name == pp_profile.variable_name:
+                    modifier_info.variable_name == pp_profile.variable_name:
                 return position
         return -1
-   
+
     def _is_opf_feasible(self):
         return self._opf.is_feasible()
-    
+
     def _get_total_operation_cost(self):
         return self._opf.get_total_cost() * self.__DAYS_PER_YEAR
 
@@ -237,39 +249,29 @@ class RecursiveFunction:
         if len(inter_iteration_information.incumbent_graph_paths) == 0:
             self._append_candidate_in_incumbent_list("0", inter_iteration_information)
         else:
-            key_path = self._check_if_candidate_path_has_been_stored_in_incumbent(\
-                inter_iteration_information)
+            key_path = check_if_candidate_path_has_been_stored_in_incumbent(inter_iteration_information)
             if key_path == "not found":
-                self._append_candidate_in_incumbent_list(str(len(\
-                    inter_iteration_information.incumbent_graph_paths)), \
-                    inter_iteration_information)
+                self._append_candidate_in_incumbent_list(str(len(inter_iteration_information.incumbent_graph_paths)),
+                                                         inter_iteration_information)
             else:
-                self._replace_incumbent_if_candidate_is_better(key_path, \
-                    inter_iteration_information)
+                self._replace_incumbent_if_candidate_is_better(key_path, inter_iteration_information)
         self._return_to_previous_state(inter_iteration_information)
 
-    def _replace_incumbent_if_candidate_is_better(self, key_in_incumbent: str, \
-        inter_iteration_information: InterIterationInformation):
-        total_cost_candidate = self._calculate_investment_cost(\
-            inter_iteration_information.candidate_interventions) +\
-            self._calculate_opteration_cost(\
-            inter_iteration_information.candidate_operation_cost)
+    def _replace_incumbent_if_candidate_is_better(self, key_in_incumbent: str,
+                                                  inter_iteration_information: InterIterationInformation):
+        total_cost_candidate = self._calculate_investment_cost( \
+            inter_iteration_information.candidate_interventions) + \
+                               self._calculate_opteration_cost( \
+                                   inter_iteration_information.candidate_operation_cost)
         total_cost_incumbent = \
-            inter_iteration_information.incumbent_investment_costs[key_in_incumbent] +\
+            inter_iteration_information.incumbent_investment_costs[key_in_incumbent] + \
             inter_iteration_information.incumbent_operation_costs[key_in_incumbent]
-        if  total_cost_incumbent > total_cost_candidate:
+        if total_cost_incumbent > total_cost_candidate:
             self._replace_solution_in_incumbent_list(key_in_incumbent, \
-            inter_iteration_information)
-    
-    def _check_if_candidate_path_has_been_stored_in_incumbent(self, \
-        inter_iteration_information: InterIterationInformation) -> str:
-        for key, value in inter_iteration_information.incumbent_graph_paths:
-            if inter_iteration_information.candidate_solution_path in value:
-                return key
-        return "not found"
-    
-    def _replace_solution_in_incumbent_list(self, key_in_incumbent: str, \
-        inter_iteration_information: InterIterationInformation):
+                                                     inter_iteration_information)
+
+    def _replace_solution_in_incumbent_list(self, key_in_incumbent: str,
+                                            inter_iteration_information: InterIterationInformation):
         inter_iteration_information.incumbent_graph_paths[key_in_incumbent] = \
             deepcopy(inter_iteration_information.candidate_solution_path)
         inter_iteration_information.incumbent_interventions[key_in_incumbent] = \
@@ -278,25 +280,27 @@ class RecursiveFunction:
             self._calculate_investment_cost(inter_iteration_information.candidate_interventions)
         inter_iteration_information.incumbent_operation_costs[key_in_incumbent] = \
             self._calculate_opteration_cost(inter_iteration_information.candidate_operation_cost)
-    
-    def _append_candidate_in_incumbent_list(self, key_in_incumbent: str, \
-        inter_iteration_information: InterIterationInformation):
-        inter_iteration_information.incumbent_graph_paths.append(key_in_incumbent, deepcopy(\
+
+    def _append_candidate_in_incumbent_list(self, key_in_incumbent: str,
+                                            inter_iteration_information: InterIterationInformation):
+        inter_iteration_information.incumbent_graph_paths.append(key_in_incumbent, deepcopy(
             inter_iteration_information.candidate_solution_path))
-        inter_iteration_information.incumbent_interventions.append(key_in_incumbent, deepcopy(\
+        inter_iteration_information.incumbent_interventions.append(key_in_incumbent, deepcopy(
             inter_iteration_information.candidate_interventions))
-        inter_iteration_information.incumbent_investment_costs.append(key_in_incumbent, \
-            self._calculate_investment_cost(inter_iteration_information.candidate_interventions))
-        inter_iteration_information.incumbent_operation_costs.append(key_in_incumbent, \
-            self._calculate_opteration_cost(inter_iteration_information.candidate_operation_cost))
-    
+        inter_iteration_information.incumbent_investment_costs. \
+            append(key_in_incumbent,
+                   self._calculate_investment_cost(inter_iteration_information.candidate_interventions))
+        inter_iteration_information.incumbent_operation_costs. \
+            append(key_in_incumbent,
+                   self._calculate_opteration_cost(inter_iteration_information.candidate_operation_cost))
+
     def _return_to_previous_state(self, inter_iteration_information: InterIterationInformation):
-        inter_iteration_information.candidate_interventions.pop(\
+        inter_iteration_information.candidate_interventions.pop( \
             str(inter_iteration_information.level_in_graph))
-        inter_iteration_information.candidate_operation_cost.pop(\
+        inter_iteration_information.candidate_operation_cost.pop( \
             str(inter_iteration_information.level_in_graph))
-        inter_iteration_information.candidate_solution_path.pop(str(len(\
-        inter_iteration_information.candidate_solution_path) - 1))
+        inter_iteration_information.candidate_solution_path.pop(str(len( \
+            inter_iteration_information.candidate_solution_path) - 1))
         inter_iteration_information.level_in_graph -= 1
 
     def _calculate_investment_cost(self, interventions: AbstractDataContainer) -> float:
@@ -305,15 +309,15 @@ class RecursiveFunction:
             partial_cost = 0.0
             for _, investment in investments:
                 partial_cost = partial_cost + investment.cost
-            total_cost = total_cost + (1/(\
-                (1-(self._parameters.problem_settings.return_rate_in_percentage/100))**year)) * \
-                partial_cost
+            total_cost = total_cost + (1 / ( \
+                        (1 - (self._parameters.problem_settings.return_rate_in_percentage / 100)) ** year)) * \
+                         partial_cost
         return total_cost
-    
+
     def _calculate_opteration_cost(self, operation_cost_per_year: AbstractDataContainer) -> float:
         total_cost = 0.0
         for year, (_, operation_cost) in enumerate(operation_cost_per_year):
-            total_cost = total_cost + (1/(\
-                (1-(self._parameters.problem_settings.return_rate_in_percentage/100))**year)) * \
-                operation_cost
+            total_cost = total_cost + (1 / ( \
+                        (1 - (self._parameters.problem_settings.return_rate_in_percentage / 100)) ** year)) * \
+                         operation_cost
         return total_cost
