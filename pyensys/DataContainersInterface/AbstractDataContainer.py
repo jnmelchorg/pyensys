c3ae537acff9012dde4c84c4b98e9d6fbc47c27e
jnmelchorg <jnmelchorguti@gmail.com>
2022-03-08 17:55:39 +0000

    added methods to get values of protected variables _is_list and _is_dictionary

--- pyensys/DataContainersInterface/AbstractDataContainer.py
+++ pyensys/DataContainersInterface/AbstractDataContainer.py
@@ -1,12 +1,13 @@
 from copy import deepcopy
 from typing import Any, Type
 
+
 class AbstractDataContainer:
     def __init__(self):
         self._container = None
         self._is_dictionary = False
-        self._is_list = False        
-    
+        self._is_list = False
+
     def __getitem__(self, key: str):
         if self._is_dictionary:
             return self._container[key]
@@ -22,81 +23,81 @@ class AbstractDataContainer:
             for item in self._container:
                 if item[0] == key:
                     item[1] = value
-    
+
     def __iter__(self):
         if self._is_dictionary:
             self._container_iterator = iter(self._container.items())
         elif self._is_list:
             self._container_iterator = iter(self._container)
         return self
-    
+
     def __next__(self):
         if self._is_dictionary:
             return next(self._container_iterator)
         elif self._is_list:
             item = next(self._container_iterator)
             return item[0], item[1]
-    
+
     def __len__(self):
         return len(self._container)
-    
+
     def __eq__(self, other):
         if isinstance(other, AbstractDataContainer):
             if (self._is_dictionary and other._is_dictionary) or \
-                (self._is_list and other._is_list):
+                    (self._is_list and other._is_list):
                 return self._container == other._container
             else:
                 raise TypeError
         else:
             raise TypeError
-    
+
     def __ne__(self, other):
         if isinstance(other, AbstractDataContainer):
             if (self._is_dictionary and other._is_dictionary) or \
-                (self._is_list and other._is_list):
+                    (self._is_list and other._is_list):
                 return self._container != other._container
             else:
                 raise TypeError
         else:
             raise TypeError
-    
+
     def __contains__(self, other):
         if isinstance(other, AbstractDataContainer):
-            if (self._is_dictionary and other._is_dictionary):
+            if self._is_dictionary and other._is_dictionary:
                 return self._compare_dictionaries(other)
-            elif (self._is_list and other._is_list):
+            elif self._is_list and other._is_list:
                 return self._compare_lists(other)
             else:
                 raise TypeError
         else:
             raise TypeError
-        
+
     def _compare_dictionaries(self, other):
         for key1 in other._container.keys():
             if (key1 not in self._container or self._container[key1] != other._container[key1]):
-                    return False
+                return False
         return True
-    
+
     def _compare_lists(self, other):
         for item in other._container:
             if item not in self._container:
                 return False
         return True
-    
+
     def create_dictionary(self):
         self._container = {}
         self._is_dictionary = True
-        
+
     def create_list(self):
         self._container = []
         self._is_list = True
-    
-    def append(self, key: str,  value: Any):
+
+    def append(self, key: str, value: Any):
         if self._is_dictionary:
             self._container[key] = value
         elif self._is_list:
             self._container.append([key, value])
-    
+
     def get(self, key: str):
         if self._is_dictionary:
             return self._container.get(key, None)
@@ -105,13 +106,13 @@ class AbstractDataContainer:
                 if item[0] == key:
                     return item[1]
             return None
-    
+
     def get_values_as_list(self) -> list:
         if self._is_dictionary:
             return list(self._container.values())
         elif self._is_list:
             return [item[1] for item in self._container]
-    
+
     def extend(self, other):
         if isinstance(other, AbstractDataContainer):
             if self._is_dictionary and other._is_dictionary:
@@ -134,3 +135,9 @@ class AbstractDataContainer:
             if item_position == -1:
                 raise KeyError
             return self._container.pop(item_position)[1]
+
+    def is_dictionary(self) -> bool:
+        return self._is_dictionary
+
+    def is_list(self) -> bool:
+        return self._is_list
