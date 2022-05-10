8a542320086932250303809ae6b5dca541963b4d
Jose Nicolas Melchor gutierrez <jnmelchorguti@gmail.com>
2022-05-10 13:35:38 +0100

    format fix

--- pyensys/managers/DistributionSystemManager.py
+++ pyensys/managers/DistributionSystemManager.py
@@ -1,6 +1,7 @@
 from pyensys.readers.ReaderDataClasses import Parameters
 from pyensys.Optimisers.RecursiveFunction import RecursiveFunction
 
+
 def identify_and_solve(parameters: Parameters):
     if parameters.problem_settings.problem_optimizer == "recursive_function":
         RF = RecursiveFunction()
