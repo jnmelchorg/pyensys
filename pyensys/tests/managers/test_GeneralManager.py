2e28401407b56ce58ebc742a8ee54a802b6e5115
Jose Nicolas Melchor gutierrez <jnmelchorguti@gmail.com>
2022-05-20 12:59:02 +0100

    Modified version of the main access test to save the solution in json file

--- pyensys/tests/managers/test_GeneralManager.py
+++ pyensys/tests/managers/test_GeneralManager.py
@@ -1,6 +1,13 @@
-from pyensys.managers.GeneralManager import main_access_function
-from pyensys.tests.test_data_paths import get_attest_json_test_data
+from os.path import join
+
+from pyensys.managers.GeneralManager import main_access_function, save_in_json
+from pyensys.tests.test_data_paths import get_attest_json_test_data, set_pandapower_test_output_directory
 
 
 def test_main_access_function():
-    main_access_function(file_path=get_attest_json_test_data())
+    # main_access_function(file_path=get_attest_json_test_data())
+    solution = main_access_function(
+        file_path="C:\\Users\\f09903jm\\git projects\\pyensys\\pyensys\\tests\\json\\attest_input_format_m1.json")
+    output_dir = set_pandapower_test_output_directory()
+    output_path = join(output_dir, "output.json")
+    save_in_json(solution, output_path)
