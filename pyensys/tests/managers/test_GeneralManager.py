a09545c51a3f5706c1ceab34fc54d066c8545e4b
Jose Nicolas Melchor gutierrez <jnmelchorguti@gmail.com>
2022-05-24 10:31:07 +0100

    change input file and time measurement included

--- pyensys/tests/managers/test_GeneralManager.py
+++ pyensys/tests/managers/test_GeneralManager.py
@@ -1,15 +1,19 @@
 from os.path import join
+import time
 
 from pyensys.managers.GeneralManager import main_access_function, save_in_json
 from pyensys.tests.test_data_paths import get_attest_json_test_data, set_pandapower_test_output_directory
 
 
 def test_main_access_function():
+    start = time.time()
     # main_access_function(file_path=get_attest_json_test_data())
     solution = main_access_function(
-        file_path="C:\\Users\\f09903jm\\git projects\\pyensys\\pyensys\\tests\\json\\attest_input_format_m1.json")
+        file_path="C:\\Users\\f09903jm\\git projects\\pyensys\\pyensys\\tests\\json\\attest_input_format_m3.json")
     output_dir = "C:\\Users\\f09903jm\\git projects\\pyensys\\pyensys\\tests\\outputs"
     output_path = join(output_dir, "output.json")
     save_in_json(solution, output_path)
+    end = time.time()
+    print(end - start)
 
 test_main_access_function()
